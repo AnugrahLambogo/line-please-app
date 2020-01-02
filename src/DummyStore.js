aa7c5775844d48a3874055d2e2f891393bb1f0f7
Anugrah Lambogo <lambogoai@gmail.com>
2020-01-02 15:02:32 -0600

    working on function of teleprompter

--- src/DummyStore.js
+++ src/DummyStore.js
@@ -1,3 +1,23 @@
-let dummyStore = {};
+let dummyStore = {
+    section1: [
+        'i fly with the stars in the sky',
+        'sect 1 line 2 sldfjds;lfsl;',
+        'sect 1 line 3 sal;fdafjds;lf'],
+    section2: [
+        'section 2 ;lafj;dladfks;fjkdsa',
+        'sect 2 line 2lsfj;dasldaffds',
+        'sect 2 line 3;lasdjadf;adfls;dsf',
+    ],
+    section3: [
+        'section 3 ;slfasdjf;dlasflkj',
+        'sect 3 line 2 l;sdfjl;sadjfldsjldsa',
+        'sect 3 line 3 ;lsdfjs;fja;sdf'
+    ],
+    section4: [
+        'section 4 ;lsdfj;asldfjd',
+        'sect 4 line 2 lsj;asldkf',
+        'sect 4 line 3 lskjdf;adjsf;djs'
+    ],
+};
 
 export default dummyStore;
--- src/telePrompt.js
+++ src/telePrompt.js
@@ -1,41 +1,100 @@
 import React from 'react';
 import './telePrompt.css';
+import dummyStore from './DummyStore';
+
 
 class telePrompt extends React.Component {
-    state= {};
+    state= {
+      data: dummyStore,
+      sectNum: 0,
+      lineNum: 0,
+      lineOnly: false
+    };
+//with the lineonly property, 
+//false means we're displaying by section, true means by line
 
-    render() {
+nextPart = () => {
+if (this.state.lineOnly === false) {
+this.setState(
+  {sectNum: this.state.sectNum + 1}
+);
+}
+
+else {
+  this.setState(
+    {lineNum: this.state.lineNum +1}
+  );
+}
+}
+
+lastPart = () => {
+  if (this.state.lineOnly === false) {
+    this.setState(
+      {sectNum: this.state.sectNum - 1}
+    );
+    }
+    
+    else {
+      this.setState(
+        {lineNum: this.state.lineNum - 1}
+      );
+    }
+    }
+
+
+toggleLineOrSection = () => {
+  this.setState(
+    {lineOnly: !this.state.lineOnly}
+  )
+}
+
+
+
+  displayText = (text, time, lineOrSection) => {
+      let sectionKeys = Object.keys(text);
+      let currentSect = sectionKeys[this.state.sectNum];
+      console.log(text[currentSect]);
+      let sectArr = text[currentSect];
+
+      if (lineOrSection === 'false') {
+          return (<li className="line"> {sectArr} </li>)
+          }
+         
+      else {
+        return (<li className="line"> {sectArr[this.state.lineNum]} </li>)
+      }
+      
+  }
+ 
+  
+  render() {
         return(
         <section>
           <h2>Teleprompt Mode</h2>
           <h3 id="currentOration">Title of oration</h3>
           
-          <label for="lineOrSection">Display by:</label>
-          <select name ="lineOrSection" id="lineOrSection">
+          <label htmlFor="lineOrSection">Display by:</label>
+          <select name ="lineOrSection" id="lineOrSection" onChange={this.toggleLineOrSection}>
             <option>Section</option>
             <option>Line</option>
             </select>
              
-          <label for="displayTime">How many seconds to display:</label>
+          <label htmlFor="displayTime">How many seconds to display:</label>
           <input type="number" name="displayTime" id="displayTime">
           </input>
+          <button id="startButton">Start/Pause</button>
          
         <section className="display" > 
 
-       {/*The text displayer will need to be it's own module,
-        pass the selection of 'lineOrSection' as a prop
-        or maybe have two different modules and depending on the selection, we use on or the other
-         */}
           <ul className="orationDisplay">  
-            <li className="line"> Lorem ipsum etc. etc.</li>
-            <li className="line"> second line lorem ipsu, </li>
-            <li className="line"> third line lorem impsum </li>
+            {this.displayText(this.state.data, 5000, this.state.lineOnly)}
           </ul>  
+
         </section>
         <section>
           <button id="pauseButton">Pause</button>
-          <button id="backButton">back</button>
-          <button id="forwardButton">next</button>
+          <button id="backButton" onClick={this.lastPart}>back</button>
+          <button id="forwardButton" onClick={this.nextPart}>next</button>
           <button id="restartButton">Restart</button>
         </section>   </section>
     );
