c371ddcdacd0019eed228213fc72d1f4a1f306d2
Anugrah Lambogo <lambogoai@gmail.com>
2020-03-19 17:39:39 -0500

    fixed more styles and landing page

--- src/landing.js
+++ src/landing.js
@@ -6,20 +6,20 @@ const landing = () => {
        <div id="landing" >
            <h3>Welcome to Line Please!</h3>
            <p>Do you need to memorize anything? <br/>
-            Line Please helps users commit any blocks of text into memory!
+            <span className="appName">Line Please!</span> helps you commit any blocks of text into memory!
             Using a teleprompt-like tool, you can have your text be displayed
             in chronological order as you recite and memorize it to heart.<br/><br/>
             As you become more familiar with your text, you can make it only show 
             the first two words of each line--prompting you to recall the rest of the line.
             </p>
-           <p> Line Please is perfect for:
-            <br/><br/>
-            Speeches<br/>
-            Monologues<br/>
-            Poetry<br/>
-            Words to a song<br/>
-            Speaking to your boss/significant other/crush/etc.<br/>
-            <br/>
+           <p> <span className="appName">Line Please!</span> is perfect for:
+            <ul>
+            <li className="landingList">Speeches</li>
+            <li className="landingList">Monologues</li>
+            <li className="landingList">Poetry</li>
+            <li className="landingList">Words to a song</li>
+            <li className="landingList">Speaking to your boss/significant other/crush/etc.</li>
+            </ul>
             Hit <Link to='/upload' className="link">Upload new</Link> to upload your text and start practicing! 
             </p>
 
